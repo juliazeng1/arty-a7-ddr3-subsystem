4324da4626bb5e0d5e779cf3928af0cc8a825525
Julia Zeng <jollyjullian@gmail.com>
2024-02-27 00:19:53 -0600

    comment out xilinx traffic generator ip

--- imports/example_top.v
+++ imports/example_top.v
@@ -660,6 +660,10 @@ module example_top #
 
   (
 
+   // GPIO Button
+
+   input btn,
+
 
 
 
@@ -1534,283 +1538,283 @@ function integer clogb2 (input integer size);
 
 
 
-   mig_7series_v4_2_axi4_tg #(
+//    mig_7series_v4_2_axi4_tg #(
 
 
 
 
 
-     .C_AXI_ID_WIDTH                   (C_S_AXI_ID_WIDTH),
+//      .C_AXI_ID_WIDTH                   (C_S_AXI_ID_WIDTH),
 
 
-     .C_AXI_ADDR_WIDTH                 (C_S_AXI_ADDR_WIDTH),
+//      .C_AXI_ADDR_WIDTH                 (C_S_AXI_ADDR_WIDTH),
 
 
-     .C_AXI_DATA_WIDTH                 (C_S_AXI_DATA_WIDTH),
+//      .C_AXI_DATA_WIDTH                 (C_S_AXI_DATA_WIDTH),
 
 
-     .C_AXI_NBURST_SUPPORT             (C_AXI_NBURST_TEST),
+//      .C_AXI_NBURST_SUPPORT             (C_AXI_NBURST_TEST),
 
 
-     .C_EN_WRAP_TRANS                  (C_EN_WRAP_TRANS),
+//      .C_EN_WRAP_TRANS                  (C_EN_WRAP_TRANS),
 
 
-     .C_BEGIN_ADDRESS                  (BEGIN_ADDRESS),
+//      .C_BEGIN_ADDRESS                  (BEGIN_ADDRESS),
 
 
-     .C_END_ADDRESS                    (END_ADDRESS),
+//      .C_END_ADDRESS                    (END_ADDRESS),
 
 
-     .PRBS_EADDR_MASK_POS              (PRBS_EADDR_MASK_POS),
+//      .PRBS_EADDR_MASK_POS              (PRBS_EADDR_MASK_POS),
 
 
-     .DBG_WR_STS_WIDTH                 (DBG_WR_STS_WIDTH),
+//      .DBG_WR_STS_WIDTH                 (DBG_WR_STS_WIDTH),
 
 
-     .DBG_RD_STS_WIDTH                 (DBG_RD_STS_WIDTH),
+//      .DBG_RD_STS_WIDTH                 (DBG_RD_STS_WIDTH),
 
 
-     .ENFORCE_RD_WR                    (ENFORCE_RD_WR),
+//      .ENFORCE_RD_WR                    (ENFORCE_RD_WR),
 
 
-     .ENFORCE_RD_WR_CMD                (ENFORCE_RD_WR_CMD),
+//      .ENFORCE_RD_WR_CMD                (ENFORCE_RD_WR_CMD),
 
 
-     .EN_UPSIZER                       (C_S_AXI_SUPPORTS_NARROW_BURST),
+//      .EN_UPSIZER                       (C_S_AXI_SUPPORTS_NARROW_BURST),
 
 
-     .ENFORCE_RD_WR_PATTERN            (ENFORCE_RD_WR_PATTERN)
+//      .ENFORCE_RD_WR_PATTERN            (ENFORCE_RD_WR_PATTERN)
 
 
 
 
 
-   ) u_axi4_tg_inst
+//    ) u_axi4_tg_inst
 
 
-   (
+//    (
 
 
-     .aclk                             (clk),
+//      .aclk                             (clk),
 
 
-     .aresetn                          (aresetn),
+//      .aresetn                          (aresetn),
 
 
 
 
 
-// Input control signals
+// // Input control signals
 
 
-     .init_cmptd                       (init_calib_complete),
+//      .init_cmptd                       (init_calib_complete),
 
 
-     .init_test                        (1'b0),
+//      .init_test                        (1'b0),
 
 
-     .wdog_mask                        (~init_calib_complete),
+//      .wdog_mask                        (~init_calib_complete),
 
 
-     .wrap_en                          (1'b0),
+//      .wrap_en                          (1'b0),
 
 
 
 
 
-// AXI write address channel signals
+// // AXI write address channel signals
 
 
-     .axi_wready                       (s_axi_awready),
+//      .axi_wready                       (s_axi_awready),
 
 
-     .axi_wid                          (s_axi_awid),
+//      .axi_wid                          (s_axi_awid),
 
 
-     .axi_waddr                        (s_axi_awaddr),
+//      .axi_waddr                        (s_axi_awaddr),
 
 
-     .axi_wlen                         (s_axi_awlen),
+//      .axi_wlen                         (s_axi_awlen),
 
 
-     .axi_wsize                        (s_axi_awsize),
+//      .axi_wsize                        (s_axi_awsize),
 
 
-     .axi_wburst                       (s_axi_awburst),
+//      .axi_wburst                       (s_axi_awburst),
 
 
-     .axi_wlock                        (s_axi_awlock),
+//      .axi_wlock                        (s_axi_awlock),
 
 
-     .axi_wcache                       (s_axi_awcache),
+//      .axi_wcache                       (s_axi_awcache),
 
 
-     .axi_wprot                        (s_axi_awprot),
+//      .axi_wprot                        (s_axi_awprot),
 
 
-     .axi_wvalid                       (s_axi_awvalid),
+//      .axi_wvalid                       (s_axi_awvalid),
 
 
 
 
 
-// AXI write data channel signals
+// // AXI write data channel signals
 
 
-     .axi_wd_wready                    (s_axi_wready),
+//      .axi_wd_wready                    (s_axi_wready),
 
 
-     .axi_wd_wid                       (s_axi_wid),
+//      .axi_wd_wid                       (s_axi_wid),
 
 
-     .axi_wd_data                      (s_axi_wdata),
+//      .axi_wd_data                      (s_axi_wdata),
 
 
-     .axi_wd_strb                      (s_axi_wstrb),
+//      .axi_wd_strb                      (s_axi_wstrb),
 
 
-     .axi_wd_last                      (s_axi_wlast),
+//      .axi_wd_last                      (s_axi_wlast),
 
 
-     .axi_wd_valid                     (s_axi_wvalid),
+//      .axi_wd_valid                     (s_axi_wvalid),
 
 
 
 
 
-// AXI write response channel signals
+// // AXI write response channel signals
 
 
-     .axi_wd_bid                       (s_axi_bid),
+//      .axi_wd_bid                       (s_axi_bid),
 
 
-     .axi_wd_bresp                     (s_axi_bresp),
+//      .axi_wd_bresp                     (s_axi_bresp),
 
 
-     .axi_wd_bvalid                    (s_axi_bvalid),
+//      .axi_wd_bvalid                    (s_axi_bvalid),
 
 
-     .axi_wd_bready                    (s_axi_bready),
+//      .axi_wd_bready                    (s_axi_bready),
 
 
 
 
 
-// AXI read address channel signals
+// // AXI read address channel signals
 
 
-     .axi_rready                       (s_axi_arready),
+//      .axi_rready                       (s_axi_arready),
 
 
-     .axi_rid                          (s_axi_arid),
+//      .axi_rid                          (s_axi_arid),
 
 
-     .axi_raddr                        (s_axi_araddr),
+//      .axi_raddr                        (s_axi_araddr),
 
 
-     .axi_rlen                         (s_axi_arlen),
+//      .axi_rlen                         (s_axi_arlen),
 
 
-     .axi_rsize                        (s_axi_arsize),
+//      .axi_rsize                        (s_axi_arsize),
 
 
-     .axi_rburst                       (s_axi_arburst),
+//      .axi_rburst                       (s_axi_arburst),
 
 
-     .axi_rlock                        (s_axi_arlock),
+//      .axi_rlock                        (s_axi_arlock),
 
 
-     .axi_rcache                       (s_axi_arcache),
+//      .axi_rcache                       (s_axi_arcache),
 
 
-     .axi_rprot                        (s_axi_arprot),
+//      .axi_rprot                        (s_axi_arprot),
 
 
-     .axi_rvalid                       (s_axi_arvalid),
+//      .axi_rvalid                       (s_axi_arvalid),
 
 
 
 
 
-// AXI read data channel signals
+// // AXI read data channel signals
 
 
-     .axi_rd_bid                       (s_axi_rid),
+//      .axi_rd_bid                       (s_axi_rid),
 
 
-     .axi_rd_rresp                     (s_axi_rresp),
+//      .axi_rd_rresp                     (s_axi_rresp),
 
 
-     .axi_rd_rvalid                    (s_axi_rvalid),
+//      .axi_rd_rvalid                    (s_axi_rvalid),
 
 
-     .axi_rd_data                      (s_axi_rdata),
+//      .axi_rd_data                      (s_axi_rdata),
 
 
-     .axi_rd_last                      (s_axi_rlast),
+//      .axi_rd_last                      (s_axi_rlast),
 
 
-     .axi_rd_rready                    (s_axi_rready),
+//      .axi_rd_rready                    (s_axi_rready),
 
 
 
 
 
-// Error status signals
+// // Error status signals
 
 
-     .cmd_err                          (cmd_err),
+//      .cmd_err                          (cmd_err),
 
 
-     .data_msmatch_err                 (data_msmatch_err),
+//      .data_msmatch_err                 (data_msmatch_err),
 
 
-     .write_err                        (write_err),
+//      .write_err                        (write_err),
 
 
-     .read_err                         (read_err),
+//      .read_err                         (read_err),
 
 
-     .test_cmptd                       (test_cmptd),
+//      .test_cmptd                       (test_cmptd),
 
 
-     .write_cmptd                      (write_cmptd),
+//      .write_cmptd                      (write_cmptd),
 
 
-     .read_cmptd                       (read_cmptd),
+//      .read_cmptd                       (read_cmptd),
 
 
-     .cmptd_one_wr_rd                  (cmptd_one_wr_rd),
+//      .cmptd_one_wr_rd                  (cmptd_one_wr_rd),
 
 
 
 
 
-// Debug status signals
+// // Debug status signals
 
 
-     .cmp_data_en                      (cmp_data_valid),
+//      .cmp_data_en                      (cmp_data_valid),
 
 
-     .cmp_data_o                       (cmp_data),
+//      .cmp_data_o                       (cmp_data),
 
 
-     .rdata_cmp                        (rdata_cmp),
+//      .rdata_cmp                        (rdata_cmp),
 
 
-     .dbg_wr_sts_vld                   (dbg_wr_sts_vld),
+//      .dbg_wr_sts_vld                   (dbg_wr_sts_vld),
 
 
-     .dbg_wr_sts                       (dbg_wr_sts),
+//      .dbg_wr_sts                       (dbg_wr_sts),
 
 
-     .dbg_rd_sts_vld                   (dbg_rd_sts_vld),
+//      .dbg_rd_sts_vld                   (dbg_rd_sts_vld),
 
 
-     .dbg_rd_sts                       (dbg_rd_sts)
+//      .dbg_rd_sts                       (dbg_rd_sts)
 
 
-);
+// );
 
 
 
